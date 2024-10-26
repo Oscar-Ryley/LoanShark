816d672bbeb3a53a76ca04a78e0c03b3d0290402
Oscar Ryley <oscar.ryley@gmail.com>
2024-10-26 23:47:28 +0100

    The Debt Dino Follows the Shark Player Character
    Used code from this tutorial: https://www.youtube.com/watch?v=5hW9A2XFm38
    
    Next stage is to allow the Dino to walk through the Maze's Walls.

--- Godot/Scripts/Dino.gd
+++ Godot/Scripts/Dino.gd
@@ -1,6 +1,20 @@
 extends CharacterBody3D
 
+const SPEED = 2.0
+var hero
+@export var turn_speed = 4.0
+
+func _ready():
+	hero = %SharkPlayer
+
 func _process(delta: float) -> void:
 	scale.x = Global.debt
 	scale.y = Global.debt
 	scale.z = Global.debt
+
+func _physics_process(delta):
+	$FaceDirection.look_at(hero.global_transform.origin, Vector3.UP)
+	rotate_y(-deg_to_rad($FaceDirection.rotation.y * turn_speed))
+	$NavigationAgent3D.set_target_position(hero.global_transform.origin)
+	var velocity = ($NavigationAgent3D.get_next_path_position() - transform.origin).normalized() * SPEED * delta
+	move_and_collide(velocity)
