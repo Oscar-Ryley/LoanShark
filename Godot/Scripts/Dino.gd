aa24ccd6cc248ba03b6b2223ecd52d6d17d39b29
Oscar Ryley <oscar.ryley@gmail.com>
2024-10-26 23:54:14 +0100

    Fixed Collision by Changing Dino's collision mask
    Game is now complete apart from win and lose conditions! I'm going to record some gameplay because the game is in a good state to show off the mechanics and atmosphere.

--- Godot/Scripts/Dino.gd
+++ Godot/Scripts/Dino.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
-const SPEED = 2.0
+const SPEED = 4.0
 var hero
-@export var turn_speed = 4.0
+@export var turn_speed = 1.0
 
 func _ready():
 	hero = %SharkPlayer
@@ -13,8 +13,9 @@ func _process(delta: float) -> void:
 	scale.z = Global.debt
 
 func _physics_process(delta):
-	$FaceDirection.look_at(hero.global_transform.origin, Vector3.UP)
-	rotate_y(-deg_to_rad($FaceDirection.rotation.y * turn_speed))
-	$NavigationAgent3D.set_target_position(hero.global_transform.origin)
+	if int(delta) % 4 == 0:
+		$FaceDirection.look_at(hero.global_transform.origin, Vector3.UP)
+		rotate_y(-deg_to_rad($FaceDirection.rotation.y * turn_speed))
+		$NavigationAgent3D.set_target_position(hero.global_transform.origin)
 	var velocity = ($NavigationAgent3D.get_next_path_position() - transform.origin).normalized() * SPEED * delta
 	move_and_collide(velocity)
